67c2ea0b036b61521688e7384761485e37549120
Marcsc777 <markryazan@mail.ru>
2020-11-30 22:21:35 +0300

    ex15_js-oop

---
+++ src/ex15_js-oop/task-01.js
@@ -0,0 +1,60 @@
+class Candies {
+  constructor(name, grams) {
+    this.name = name;
+    this.grams = grams;
+  }
+}
+
+class Lollipops {
+  constructor(name, grams) {
+    this.name = name;
+    this.grams = grams;
+  }
+}
+
+const twix = new Candies(`Twix`, 55);
+const mars = new Candies(`Mars`, 70);
+const kitKat = new Candies(`KitKat`, 35);
+const picnic = new Candies(`Picnic`, 80);
+const barberry = new Lollipops(`Barberry`, 20);
+
+class Gift {
+  constructor(gift) {
+    this.gift = gift;
+  }
+  
+  determineWeight() {
+    let summ = 0;
+    
+    for (let i = 0; i < this.gift.length; i++) {
+      summ +=  this.gift[i].grams;
+    }
+    
+    return `Общий вес подарка: ${summ}`;
+  }
+  
+  sortSimple() {
+    this.Arr = [];
+    
+    for (let i = 0; i < this.gift.length; i++) {
+      if (this.gift[i].grams < 40) {
+        this.Arr.push(this.gift[i].name);
+      }
+    }
+    
+    return `Сладости, вес которых менее 40г весом: ${this.Arr}`;
+  }
+  
+  hasCandy (nameCandies) {
+    return this.gift.filter(function(value) {
+      return value.name === nameCandies;
+    });
+  }
+}
+
+let gift = new Gift([twix, mars, mars, kitKat, picnic, barberry]);
+
+console.log(gift);
+console.log(gift.determineWeight());
+console.log(gift.sortSimple());
+console.log(gift.hasCandy(`Mars`));
---
+++ src/ex15_js-oop/task-02.js
@@ -0,0 +1,37 @@
+function Appliences(name, powerConsumption) {
+  this.name = name;
+  this.powerConsumption = powerConsumption;
+}
+
+function Room (name) {
+  this.name = name;
+}
+
+Room.prototype.pluggetIn = function(pluggetIn) {
+  this.pluggetIn = pluggetIn;
+};
+Room.prototype.determinePower = function() {
+  let summ = 0;
+  for (let i = 0; i < room.pluggetIn.length; i++) {
+    summ +=  room.pluggetIn[i].powerConsumption;
+  }
+  
+  return `Мощность электроприборов в комнате: ${summ} Вт`;
+}
+Room.prototype.hasName = function(nameAppliences) {
+  return this.name.filter(function(value) {
+    return value.name === nameAppliences;
+  })
+};
+let toaster = new Appliences(`Toaster`, 800);
+let coffeeMaker = new Appliences(`Coffee maker`, 750);
+let deepFryer = new Appliences(`Deep fryer`, 1500);
+let kettle = new Appliences(`Kettle`, 500);
+let room = new Room([toaster, coffeeMaker, coffeeMaker, deepFryer, kettle]);
+
+room.pluggetIn([coffeeMaker, toaster]);
+
+console.log(room);
+console.log(room.pluggetIn);
+console.log(room.determinePower());
+console.log(room.hasName(`Coffee maker`));
